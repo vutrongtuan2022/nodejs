4f59b90916347ee3a71b9ef14cf15ee02899042e
TuanVT <vutrongtuan@gmail.com>
2024-10-21 17:18:11 +0700

    ff

---
+++ src/redux/reducer/site.js
@@ -0,0 +1,24 @@
+import createSlice from "@reduxjs/toolkit";
+const initialState = {
+  loading: true,
+  isMobile: false,
+  isRememberPassword: false,
+};
+export const siteSlice = createSlice({
+  name: "site",
+  initialState,
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action?.payload;
+    },
+    setIsMobile: (state, action) => {
+      state.isMobile = action.payload;
+    },
+    setRememberPassword: (state, action) => {
+      state.isRememberPassword = action.payload;
+    },
+  },
+});
+export const { setLoading, setIsMobile, setRememberPassword } =
+  siteSlice.action;
+export default siteSlice.reducer;
