6aaca7c88b349cad54e20f7fafbfc610452ae382
TuanVT <vutrongtuan@gmail.com>
2025-01-20 10:29:08 +0700

    ff

--- services/blockMVC/src/index.js
+++ services/blockMVC/src/index.js
@@ -32,6 +32,6 @@ app.set('views', path.join(__dirname, 'resources/views')); // Đặt đúng đư
 router(app);
 
 // Start server
-              app.listen(port, () => {
-                console.log(`Example app listening on port ${port}`);
-              });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
